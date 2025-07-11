7543081e3f4dddda67c44453811d66720efa33db
Burgess-T <burgess_t@foxmail.com>
2025-07-11 08:58:29 +0800

    Update Delete_File.bat
    修复通配符无法删除的问题

--- Delete_File.bat
+++ Delete_File.bat
@@ -6,9 +6,17 @@ setlocal enabledelayedexpansion
 set /p "FILE_PATTERNS=Enter the filename pattern (e.g. test.txt, *.tmp): "
 
 echo Recursively deleting all matching files (including hidden files)...
-for %%p in (%FILE_PATTERNS%) do (
-    echo Processing pattern: "%%p"
-    for /f "delims=" %%i in ('dir /s /b /a:-D "%%p" 2^>nul') do (
+
+:: Build quoted patterns to prevent premature expansion
+set "quoted_patterns="
+for %%a in (%FILE_PATTERNS%) do (
+    set "quoted_patterns=!quoted_patterns! "%%a""
+)
+
+for %%p in (!quoted_patterns!) do (
+    set "pattern=%%~p"
+    echo Processing pattern: "!pattern!"
+    for /f "delims=" %%i in ('dir /s /b /a:-D "!pattern!" 2^>nul') do (
         attrib -h -s "%%i" >nul 2>&1
         del /f /q "%%i" >nul
         if exist "%%i" (
